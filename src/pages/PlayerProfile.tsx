c540daee984c0cc96201aa95f023ea6600bfa67d
earlcharless <sario.earlcharles@yahoo.com>
2025-06-18 18:52:02 +0800

    initial mapping of info display in ui

--- src/pages/PlayerProfile.tsx
+++ src/pages/PlayerProfile.tsx
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { format, intervalToDuration } from "date-fns";
 import { useParams } from "react-router-dom";
 import { getPlayerProfile } from "../services/playersService.ts";
 
 const PlayerProfile: React.FC = () => {
   const { userName } = useParams();
   const [profile, setProfile] = useState<any>();
+  const lastOnlineRef = useRef<HTMLSpanElement>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       if (userName) {
         const { data } = await getPlayerProfile(userName);
 
-        console.log(data);
-
         if (data) {
           const formattedData: any = {...data};
           
           const dateJoined = new Date(formattedData.joined * 1000);
-          console.log({ dateJoined });
-          formattedData.joined = dateJoined.toLocaleDateString();
+          formattedData.joined = format(dateJoined, "MMM d, yyyy");
+
           setProfile(formattedData);
         }
       }
@@ -26,6 +26,24 @@ const PlayerProfile: React.FC = () => {
 
     fetchData();
   }, [userName]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const dateTimeNow = new Date();
+      const lastOnline = new Date(profile.last_online * 1000);
+      const duration = intervalToDuration({ start: lastOnline, end: dateTimeNow });
+      const hours = String(duration.hours ?? 0).padStart(2, '0');
+      const minutes = String(duration.minutes ?? 0).padStart(2, '0');
+      const seconds = String(duration.seconds ?? 0).padStart(2, '0');
+
+      if (lastOnlineRef.current) {
+        lastOnlineRef.current.textContent  = `${hours}:${minutes}:${seconds}`;
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [profile]);
+
 
   if (!profile) {
     return <>Loading</>
@@ -43,10 +61,14 @@ const PlayerProfile: React.FC = () => {
       is_streamer: {profile.is_streamer ? "TRUE" : "FALSE"}  <br />
 
 
-      joined: {profile.joined}  <br />
+      joined: {profile.joined}  <br /><br />
 
       
+      last_online_duration: <span ref={lastOnlineRef}></span> <br />
       last_online: {profile.last_online} <br />
+      now: {profile.now} <br />
+      <br />
+      
       league: {profile.league} <br />
       location: {profile.location}  <br />
       name: {profile.name}  <br />
